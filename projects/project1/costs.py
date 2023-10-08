6f2b1c119ea20a5dcf82edc40485be8fb78a34ba
Ugo Balducci <51507845+TheGuyWithoutH@users.noreply.github.com>
2023-10-08 18:35:09 +0200

    add functions

---
+++ projects/project1/costs.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""a function used to compute the loss."""
+
+import numpy as np
+
+
+def compute_loss(y, tx, w):
+    """Calculate the loss using either MSE or MAE.
+
+    Args:
+        y: shape=(N, )
+        tx: shape=(N,2)
+        w: shape=(2,). The vector of model parameters.
+
+    Returns:
+        the value of the loss (a scalar), corresponding to the input parameters w.
+    """
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # TODO: compute loss by MSE
+    # ***************************************************
+    error = y - tx.dot(w)
+    return  np.sum(error**2) / (2 * len(y))
+
+    # INSERT YOUR CODE HERE
+    # TODO: compute loss by MAE
+    # ***************************************************
+    # error = y - tx.dot(w)
+    # return np.sum(np.abs(error)) / len(y)
