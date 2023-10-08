81aa6549bd5642a0225269b311dc4e66f7add2f4
Khadija Tagemouati <khadija.tagemouati@epfl.ch>
2023-10-08 18:39:10 +0200

    add functions for least squares and ridge regression

---
+++ projects/project1/ridge_regression.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def ridge_regression(y, tx, lambda_):
+    """implement ridge regression.
+
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+        lambda_: scalar.
+
+    Returns:
+        w: optimal weights, numpy array of shape(D,), D is the number of features.
+
+    >>> ridge_regression(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), 0)
+    array([ 0.21212121, -0.12121212])
+    >>> ridge_regression(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), 1)
+    array([0.03947092, 0.00319628])
+    """
+    D = tx.shape[1]
+    N = tx.shape[0]
+    A = tx.T@tx + lambda_*2*N*np.identity(D)
+    b = tx.T@y
+    return np.linalg.solve(A, b)
