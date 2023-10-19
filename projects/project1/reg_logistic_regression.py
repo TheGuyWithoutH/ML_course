e1e35519577dfaf6cc17226e8981efab5749130c
Ghita2002 <ghita.tagemouati@epfl.ch>
2023-10-19 15:11:44 +0100

    Adding reg_logistic_regression method

--- projects/project1/reg_logistic_regression.py
+++ projects/project1/reg_logistic_regression.py
@@ -5,7 +5,6 @@ def reg_logistic_regression(y, tx, lambda_,initial_w, max_iters, gamma):
     """Regularized logistic regression using gradient descent or SGD"""
     # init parameters
     #threshold = 1e-8
-    gradient= compute_gradient(y, tx, initial_w, max_iters, gamma)
     """ losses = []
     w = initial_w
     # start the logistic regression
@@ -19,6 +18,11 @@ def reg_logistic_regression(y, tx, lambda_,initial_w, max_iters, gamma):
         losses.append(loss)
         if len(losses) > 1 and np.abs(losses[-1] - losses[-2]) < threshold:
             break """
-    N = tx.shape[0]
-    a = initial_w- (1/N)*np.sum(np.log(1+np.exp(tx@initial_w))-y@tx@initial_w)+lambda_*np.linalg.norm(initial_w)**2
-    raise NotImplementedError
+    ws=[initial_w]
+    w=initial_w
+    for i in range(max_iters):
+        loss = compute_loss(y, tx, w) + lambda_*np.squeeze(w.T@w)
+        gradient = compute_gradient(y, tx, w, max_iters, gamma) + 2*lambda_*w
+        w = w - gamma*gradient
+        ws.append(w)
+    return ws, loss
